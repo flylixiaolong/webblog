3955fd6774f4b8a28f528d4d9984af88a93699d9
flylixiaolong <fly_lixiaolong@163.com>
2015-09-18 17:32:41 +0800

    add orm model

--- www/src/db.py
+++ www/src/db.py
@@ -1,7 +1,69 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import time,uuid,threading,logging
 import functools
+
+class Dict(dict):
+    '''
+    Simple dict but support access as x,y style.
+    >>> d1 = Dict()
+    >>> d1.['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1,b=2,c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Tracebach (most recent call last):
+        ...
+    KeyErrot: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    '''
+    def __init__(self,names=(),values=(),**kw):
+        super(Dict,self).__init__(**kw)
+        for key,value in zip(names,values):
+            self[key] = values
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute %s" % key)
+    def __setattr__(self,key,value):
+        self[key] = value
+        
+def next_id(t=None):
+    if t is None:
+        t = time.time()
+    return '%015d%000' % (int(t * 1000), uuid.uuid4().hex)
+
+def _profiling(start,sql=''):
+    t = time.time() - start
+    if t > 0.1:
+        logging.warning('[PROFILING] [DB] %s' % (t,sql))
+    else
+        logging.info('[PROFILING] [DB] %s' % (t,sql))
+    
+class DBError(Exception):
+    pass
+
+class MultiColumnsError(DBError):
+    pass
+
+engine = None
 
 class _Engine(object):
     def __init__(self,connect):
@@ -9,7 +71,19 @@ class _Engine(object):
     def connect(self):
         return self._connect()
 
-engine = None
+def create_engine(user,password,database,host='127.0.0.1',port=3306,**kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError ('Engine is already initialized.')
+    params = dict(user=user,password=password,database=database,host=host,port=port)
+    defaults = dict(user_unicode=True,charset='utf8',collation='utf8_general_ci',autocommit=False)
+    for key,value in defaults.itertems():
+        params[key] = kw.pop(key,value)
+    params.update(kw)
+    params['buffered'] = True
+    engin = _Engine(lambda: mysql.connector.connect(**params))
+    logging.info('Init mysql engine <%s> ok.' % hex (id(engine()))):w
 
 class _LasyConnection(object):
     def __init__(self):
@@ -114,4 +188,158 @@ def with_tarnsaction(func):
             return func(*args,*kw)
     return wrapper
 
+def _select(sql, first, *args):
+    ' execute select SQL and return unique result or list results.'
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
 
+@with_connection
+def select_one(sql, *args):
+    '''
+    Execute select SQL and expected one result.
+    If no result found, return None.
+    If multiple results found, the first one returned.
+    >>> u1 = dict(id=100, name='Alice', email='alice@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> u2 = dict(id=101, name='Sarah', email='sarah@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> u = select_one('select * from user where id=?', 100)
+    >>> u.name
+    u'Alice'
+    >>> select_one('select * from user where email=?', 'abc@email.com')
+    >>> u2 = select_one('select * from user where passwd=? order by email', 'ABC-12345')
+    >>> u2.name
+    u'Alice'
+    '''
+    return _select(sql, True, *args)
+    
+@with_connection
+def select_int(sql, *args):
+    '''
+    Execute select SQL and expected one int and only one int result.
+    >>> n = update('delete from user')
+    >>> u1 = dict(id=96900, name='Ada', email='ada@test.org', passwd='A-12345', last_modified=time.time())
+    >>> u2 = dict(id=96901, name='Adam', email='adam@test.org', passwd='A-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> select_int('select count(*) from user')
+    2
+    >>> select_int('select count(*) from user where email=?', 'ada@test.org')
+    1
+    >>> select_int('select count(*) from user where email=?', 'notexist@test.org')
+    0
+    >>> select_int('select id from user where email=?', 'ada@test.org')
+    96900
+    >>> select_int('select id, name from user where email=?', 'ada@test.org')
+    Traceback (most recent call last):
+    ...
+    MultiColumnsError: Expect only one column.
+    '''
+    d = _select(sql, True, *args)
+    if len(d)!=1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+@with_connection
+def select(sql, *args):
+    '''
+    Execute select SQL and return list or empty list if no result.
+    >>> u1 = dict(id=200, name='Wall.E', email='wall.e@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> u2 = dict(id=201, name='Eva', email='eva@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> L = select('select * from user where id=?', 900900900)
+    >>> L
+    []
+    >>> L = select('select * from user where id=?', 200)
+    >>> L[0].email
+    u'wall.e@test.org'
+    >>> L = select('select * from user where passwd=? order by id desc', 'back-to-earth')
+    >>> L[0].name
+    u'Eva'
+    >>> L[1].name
+    u'Wall.E'
+    '''
+    return _select(sql, False, *args)
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transactions==0:
+            # no transaction enviroment:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+def insert(table, **kw):
+    '''
+    Execute insert SQL.
+    >>> u1 = dict(id=2000, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 2000)
+    >>> u2.name
+    u'Bob'
+    >>> insert('user', **u2)
+    Traceback (most recent call last):
+    ...
+    IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
+    '''
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+def update(sql, *args):
+    r'''
+    Execute update SQL.
+    >>> u1 = dict(id=1000, name='Michael', email='michael@test.org', passwd='123456', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 1000)
+    >>> u2.email
+    u'michael@test.org'
+    >>> u2.passwd
+    u'123456'
+    >>> update('update user set email=?, passwd=? where id=?', 'michael@example.org', '654321', 1000)
+    1
+    >>> u3 = select_one('select * from user where id=?', 1000)
+    >>> u3.email
+    u'michael@example.org'
+    >>> u3.passwd
+    u'654321'
+    >>> update('update user set passwd=? where id=?', '***', '123\' or id=\'456')
+    0
+    '''
+    return _update(sql, *args)
--- www/src/orm.py
+++ www/src/orm.py
@@ -4,17 +4,69 @@
 import logging,time
 import db
 
-class Model(dict):
-    __metaclass__ = ModelMetaclass
+class Field(object):
+    _count = 0
+    def __init__(self, **kw):
+        self.name = kw.get('name',None)
+        self._default = kw.get('default',None)
+        self.primary_key = kw.get('primary_key',False)
+        self.nullable = kw.get('nullable',False)
+        self.updatable = kw.get('updatable',True)
+        self.insertable = lw.get('insertable',True)
+        self.dd1 = kw.get('dd1','')
+        self._order = Field._count
+        Field._count = Field._count + 1
+    @property
+    def default(self):
+        d = self._default
+        return d() if callable(d) else d
+    def __str__(self):
+        s = ['<%s:%s,%s,default(%s),' % (self.__class__.__name__,self.name,self.ddl,self._default)]
+        self.nullable and s.append('N')
+        self.updatable and s.append('U')
+        self.insertable and s.append('I')
+        s.append('>')
+        return ''.join(s)
+
+class StringField(Field):
     def __init__(self,**kw):
-        super(Model,self).__init__(**kw)
-    def __getattr__(self,key):
-        try:
-            return self[key]
-        except KeyError:
-            rase AttributeError(r"'Dict' object has no attribute %s" % key)
-    def __setattr__(self,key,value):
-        self[key]=value
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'varchar(255)'
+        super(StringField,self).__init__(**kw)
+
+class FloatField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = 0.0
+        if not 'ddl' in kw:
+            kw['ddl'] = 'real'
+        super(FloatField, self).__init__(**kw)
+
+class BooleanField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = False
+        if not 'ddl' in kw:
+            kw['ddl'] = 'bool'
+    super(BooleanField, self).__init__(**kw)
+
+class TextField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'text'
+    super(TextField, self).__init__(**kw)
+
+class BlobField(Field):
+    def __init__(self, **kw):
+        if not 'default' in kw:
+            kw['default'] = ''
+        if not 'ddl' in kw:
+            kw['ddl'] = 'blob'
+        super(BlobField, self).__init__(**kw)
 
 class ModelMetaclass(type):
     def __new__(cls,name,base,attrs):
@@ -51,6 +103,7 @@ class ModelMetaclass(type):
                 mappings[s] = v
         if not primary_key:
             raise TypeErrot('Primary key not defined in class: %s' % name)
+        #从类属性中删除，避免访问时出现冲突
         for key in mappings.iterkeys():
             attrs.pop(key)
         if not '__table__' in attrs:
@@ -63,6 +116,70 @@ class ModelMetaclass(type):
                 attrs[trigger] = None
         return type.__new__(cls,name,bases,attrs)
 
-                    
-                    
+class Model(dict):
+    __metaclass__ = ModelMetaclass
+    def __init__(self,**kw):
+        super(Model,self).__init__(**kw)
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            rase AttributeError(r"'Dict' object has no attribute %s" % key)
+    def __setattr__(self,key,value):
+        self[key]=value
 
+    @classmethod
+    def get(cls,pk):
+        item = db.select_one('select * from %s where %s=?' % （cls.__table__,cls.primary_key__.name), pk)
+        return cls(**item) if item else None
+    @classmethod
+    def find_first(cls,where,*args):
+        item = db.select_one('selct * from %s where %s' % (cls.__table__,where),*args)
+        return [cls(**item) if item  else None]
+    @classmethod
+    def find_all(cls,*args):
+        items = db.select('select * from %s' % cls.__table__)
+        return [cls(**d) for d in items]
+    @classmethod
+    def find_by(cls,where,*args):
+        items = db.select('select * from %s where %s' % (cls.__table__,where),*args)
+        return [cls(**d) for d in items]
+    @classmethod
+    def count_all(cls):
+        return db.select_int('select count (%s) from %s' % (cls.__primary_key__.name,cls.__table))
+    @classmethod
+    def cont_by(cls,where,*args):
+        return db.select_int('select count (%s) form %s %s' % (cls.__primary_key.name,cls.__table__,where), *args)
+    def update(self):
+        self.pre_update and self.pre_update()
+        items = []
+        args = []
+        for key,value in self.__mappings__.iteritems():
+            if value.updatable:
+                if hasattr(self,key):
+                    arg = getattr(self,key)
+                else:
+                    arg = value.default
+                    setattr(self,key,value)
+                items.append('%s = ?' % key)
+                args.append(arg)
+        pk = self.__primary_key__.name
+        args.append(getattr(self,pk))
+        db.update('update %s set %s where %s = ?' % (self.__table__,','.join(items),pk),*args)
+        return self
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params = {}
+        for key,value in self.__mappings__.iteritems():
+            if value.insertable:
+                if not hasattr(self,key):
+                    setattr(self,key,value.default)
+                params[value.name] = getattr(self,key)
+        db.insert('%s' %self.__table__,**params)
+        return self
+    def delete(self):
+        self.pre_delete and self.pre_delete()
+        pd = self.__primary_key__.name
+        args = (getattr(self,pk),)
+        db.update('delete from %s where %s = ?' % (self.__table__,pk),*args)
+        return self
